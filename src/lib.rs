8b547780cf69bc38b727b921ef9c8e62ca7f98a7
Ben Ashford <ben@bcash.org>
2014-12-13 18:57:16 +0000

    In-place sort

--- src/lib.rs
+++ src/lib.rs
@@ -23,62 +23,94 @@ use std::rand::{task_rng, Rng};
 
 use test::Bencher;
 
-pub struct LazySortIterator<'a, T> {
-    work: Vec<Vec<T>>,
+fn pivot(lower: uint, upper: uint) -> uint {
+    return lower + ((upper - lower) / 2);
+}
+
+pub struct LazySortIterator<'a, T: Clone + 'a> {
+    data: Vec<T>,
+    work: Vec<(uint, uint)>,
     by: |&T, &T|:'a -> Ordering
 }
 
-impl <'a, T> LazySortIterator<'a, T> {
-    fn new(work: Vec<T>, by: |&T, &T|:'a -> Ordering) -> LazySortIterator<'a, T> {
+impl <'a, T: Clone> LazySortIterator<'a, T> {
+    fn new(data: Vec<T>, by: |&T, &T|:'a -> Ordering) -> LazySortIterator<'a, T> {
+        let l = data.len();
         LazySortIterator {
-            work: vec![work],
+            data: data,
+            work: vec![(0, l - 1)],
             by: by
         }
     }
+
+    fn partition(&mut self, lower: uint, upper: uint, p: uint) -> uint {
+        assert!(lower <= upper);
+        assert!(p >= lower);
+        assert!(p <= upper);
+
+        let length = upper - lower;
+        if length == 0 {
+            p
+        } else {
+            let lasti = upper;
+            let (mut i, mut nextp) = (lower, lower);
+            self.data.swap(lasti, p);
+            while i < lasti {
+                if (self.by)(&self.data[i], &self.data[lasti]) == Less {
+                    self.data.swap(i, nextp);
+                    nextp = nextp + 1;
+                }
+                i = i + 1;
+            }
+            self.data.swap(nextp, lasti);
+            nextp
+        }
+    }
+
+    fn qsort(&mut self, lower: uint, upper: uint) -> T {
+        assert!(lower <= upper);
+
+        if lower == upper {
+            return self.data[lower].clone();
+        }
+
+        let p = pivot(lower, upper);
+        let p = self.partition(lower, upper, p);
+
+        if p < upper {
+            self.work.push((p + 1, upper));
+        }
+        self.qsort(lower, p)
+    }
 }
 
-pub trait Sorted<'a, O: Ord> {
+pub trait Sorted<'a, O: Ord + Clone> {
     fn sorted(&'a mut self) -> LazySortIterator<O>;
 }
 
-pub trait SortedBy<'a, T> {
+pub trait SortedBy<'a, T: Clone> {
     fn sorted_by(&'a mut self, |&T, &T|:'a -> Ordering) -> LazySortIterator<T>;
 }
 
-impl <'a, O: Ord, I: Iterator<O>> Sorted<'a, O> for I {
+impl <'a, O: Ord + Clone, I: Iterator<O>> Sorted<'a, O> for I {
     fn sorted(&'a mut self) -> LazySortIterator<O> {
         LazySortIterator::new(self.collect(),
                               |a, b| a.cmp(b))
     }
 }
 
-impl <'a, T, I: Iterator<T>> SortedBy<'a, T> for I {
+impl <'a, T: Clone, I: Iterator<T>> SortedBy<'a, T> for I {
     fn sorted_by(&'a mut self, by: |&T, &T|:'a -> Ordering) -> LazySortIterator<T> {
         LazySortIterator::new(self.collect(), by)
     }
 }
 
-impl <'a, T> Iterator<T> for LazySortIterator<'a, T> {
+impl <'a, T: Clone> Iterator<T> for LazySortIterator<'a, T> {
     fn next(&mut self) -> Option<T> {
         match self.work.pop() {
             Some(next_work) => {
-                let mut work = next_work;
-                loop {
-                    let pivot = work.remove(0).unwrap();
-                    let (before, after) = work.partition(|v| (self.by)(v, &pivot) == Less);
-                    if before.len() == 0 {
-                        if after.len() > 0 {
-                            self.work.push(after);
-                        }
-                        return Some(pivot)
-                    } else {
-                        if after.len() > 0 {
-                            self.work.push(after);
-                        }
-                        self.work.push(vec![pivot]);
-                        work = before;
-                    }
-                }
+                let (lower, upper) = next_work;
+                Some(self.qsort(lower, upper))
             },
             None => None
         }
@@ -88,11 +120,29 @@ impl <'a, T> Iterator<T> for LazySortIterator<'a, T> {
 // TESTS
 
 #[test]
+fn details_test() {
+    let before: Vec<uint> = vec![1u, 2u, 1u, 2u, 1u, 2u, 1u, 3u, 0u];
+    let mut iter1 = before.iter();
+    let mut iter = iter1.sorted();
+
+    loop {
+        match iter.next() {
+            Some(val) => {
+                println!("NEXT: {}, DATA: {}, WORK: {}", val, iter.data, iter.work);
+            },
+            None => { break; }
+        }
+    }
+    //assert!(false);
+}
+
+#[test]
 fn sorted_test() {
     let expected: Vec<uint> = vec![1u, 1, 1, 3, 4, 6, 7, 9, 22];
     let before: Vec<uint> = vec![9u, 7, 1, 1, 6, 3, 1, 4, 22];
     let after: Vec<uint> = before.iter().sorted().map(|x| *x).collect();
 
+    println!("AFTER {}", after);
     assert_eq!(expected, after);
 }
 
@@ -108,6 +158,7 @@ fn sorted_by_test() {
     let after: Vec<&str> = before.iter()
         .sorted_by(|a, b| a.a.partial_cmp(&b.a).unwrap()).map(|x| x.b).collect();
 
+    println!("AFTER {}", after);
     assert_eq!(expected, after);
 }
 
